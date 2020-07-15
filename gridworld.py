fix broadcasting bug in q-learning
was ending up with targets of size (bsize x bsize) due to broadcasting,
which broke learning

gridworld.py
@@ -13,10 +13,10 @@ SIZE = 10
 one_hot_10 = jax.partial(utils.one_hot, k=SIZE)
 
 ACTION_MAP = jnp.stack([
-    jnp.array((1, 0), dtype=DTYPE),
-    jnp.array((0, 1), dtype=DTYPE),
-    jnp.array((-1, 0), dtype=DTYPE),
-    jnp.array((0, -1), dtype=DTYPE),
+    jnp.array((1, 0), dtype=DTYPE),   # visually DOWN
+    jnp.array((0, 1), dtype=DTYPE),   # visually RIGHT
+    jnp.array((-1, 0), dtype=DTYPE),  # visually UP
+    jnp.array((0, -1), dtype=DTYPE),  # visually LEFT
 ])
 
 
@@ -32,8 +32,11 @@ def goal(s):
     return jnp.array((s - 1, s - 1), dtype=DTYPE)
 
 
-def new(size):
-    _render = jax.partial(utils.one_hot, k=size)
+def new(size, render_onehot=True):
+    if render_onehot:
+        _render = jax.partial(utils.one_hot, k=size)
+    else:
+        _render = lambda x: x
     return GridWorld(size, _render)
 
 
@@ -59,6 +62,12 @@ def step(gridworld, action):
     return gridworld, render(gridworld), reward
 step = jax.jit(step, static_argnums=(1,))  # noqa: E305
 step_batch = jax.vmap(step)
+
+
+def all_coords(size):
+    grid = jnp.stack(jnp.meshgrid(jnp.linspace(0, size - 1, size),
+                                  jnp.linspace(0, size - 1, size)))
+    return grid.transpose().reshape(-1, 2).astype(DTYPE)
 
 
 if __name__ == "__main__":

q_learning.py
@@ -3,15 +3,17 @@ import math
 import time
 import numpy as np
 from typing import Any
+import matplotlib.pyplot as plt
 
 import jax
-from jax import numpy as jnp, random, jit, lax, profiler
+from jax import numpy as jnp, random, lax, profiler
 
 import flax
 from flax import nn, optim, struct
 
 import gridworld
 import replay_buffer
+import utils
 
 
 @struct.dataclass
@@ -40,6 +42,37 @@ class DenseQNetwork(nn.Module):
         return q
 
 
+class TabularQ(nn.Module):
+    def apply(self, s, a, env_size):
+        def init(key, shape, dtype=jnp.float32):
+            return jnp.full(shape, 97, dtype=dtype)
+
+        table = self.param('table', (env_size, env_size, 4), init)
+
+        def lookup(table, s, a):
+            x, y = s.argmax(axis=1)
+            a = a.astype(jnp.int16)
+            # a = a.reshape(-1)
+            return table[x][y][a]
+        batched_lookup = jax.vmap(lookup, in_axes=(None, 0, 0))
+        return batched_lookup(table, s, a)
+
+
+def init_fn(seed, state_shape, action_shape, tabular=False, env_size=None):
+    rng = random.PRNGKey(seed)
+    if tabular:
+        q_net = TabularQ.partial(env_size=env_size)
+    else:
+        q_net = DenseQNetwork.partial(hidden_layers=2,
+                                      hidden_dim=512)
+    _, initial_params = q_net.init_by_shape(
+        rng, [(state_shape, jnp.float32), (action_shape, jnp.float32)])
+    rng = random.split(rng, 1)[0]
+    initial_model = nn.Model(q_net, initial_params)
+    q_opt = optim.Adam(1e-2).create(initial_model)
+    return QLearnerState(q_opt, rng)
+
+
 def loss_fn(model, states, actions, targets):
     preds = model(states, actions)
     loss = jnp.mean((preds - targets)**2)
@@ -47,21 +80,8 @@ def loss_fn(model, states, actions, targets):
 grad_loss_fn = jax.grad(loss_fn)  # noqa: E305
 
 
-def init_fn(seed, state_shape, action_shape):
-    rng = random.PRNGKey(seed)
-    q_net = DenseQNetwork.partial(hidden_layers=2,
-                                  hidden_dim=512)
-    _, initial_params = q_net.init_by_shape(
-        rng, [(state_shape, jnp.float32), (action_shape, jnp.float32)])
-    rng = random.split(rng, 1)[0]
-    initial_model = nn.Model(q_net, initial_params)
-    q_opt = optim.Adam(1e-3).create(initial_model)
-    return QLearnerState(q_opt, rng)
-
-
 @jax.jit
 def train_step(q_state: QLearnerState, states, actions, targets):
-    # batch should be of form (states, actions, targets)
     grad = grad_loss_fn(q_state.model, states, actions, targets)
     q_state = q_state.replace(optimizer=q_state.optimizer.apply_gradient(grad))
     return q_state
@@ -71,45 +91,112 @@ def train_step(q_state: QLearnerState, states, actions, targets):
 @jax.jit
 def bellman_train_step(q_state: QLearnerState,
                        targetq_state: QLearnerState,
-                       transitions):
+                       transitions,
+                       candidate_actions,
+                       discount=0.99):
     # transitions should be of form (states, actions, next_states, rewards)
-    targetq_preds = targetq_state.model(transitions[0], transitions[1])
-    q_targets = transitions[3] + 0.99 * targetq_preds
+    # candidate_actions should be of form bsize x n_cands x *action_dim
+    bsize, n_candidates = candidate_actions.shape[:2]
+
+    # compute max_a' Q_t (s', a')
+    targetq_preds = predict_action_values_batch(
+        targetq_state, transitions[2], candidate_actions)
+    targetq_preds = targetq_preds.max(axis=-1).reshape(transitions[3].shape)
+    # import ipdb; ipdb.set_trace()
+
+    # compute targets and update
+    q_targets = transitions[3] + discount * targetq_preds
     return train_step(q_state, transitions[0], transitions[1], q_targets)
 
 
-def predict(q_state: QLearnerState, states, actions):
+@jax.jit
+def tabular_train_step(q_state: QLearnerState,
+                       targetq_state: QLearnerState,
+                       transitions,
+                       candidate_actions,
+                       discount=0.99):
+    value_preds = predict_action_values_batch(
+        q_state, transitions[2], candidate_actions)
+    value_preds = value_preds.max(axis=-1).reshape(transitions[3].shape)
+
+    # the next line is wrong because rewards are (128, 1) and preds are (128,)
+    # raise NotImplementedError
+    q_targets = transitions[3] + discount * value_preds
+
+    # import ipdb; ipdb.set_trace()
+    state_coords = transitions[0].argmax(axis=-1)
+    a_s = transitions[1].astype(jnp.int16)
+    for coord, a, t in zip(state_coords, a_s, q_targets):
+        x, y = coord
+        a = a[0]
+        t = t[0]
+        # import ipdb; ipdb.set_trace()
+        new_table = jax.ops.index_update(q_state.model.params['table'],
+                                         jax.ops.index[x, y, a],
+                                         t)
+        new_model = q_state.model.replace(params={'table': new_table})
+        new_optimizer = q_state.optimizer.replace(target=new_model)
+        q_state = q_state.replace(optimizer=new_optimizer)
+    return q_state
+
+
+def predict_value(q_state: QLearnerState, states, actions):
+    """Takes a batch of states and actions and returns the predicted value.
+    Returns:
+    - an ndarray of dimension `states.shape[0] == actions.shape[0]` containing
+        Q_{\theta}(s, a) for each `s` and `a` in the batch.
+    """
     return q_state.model(states, actions)
 
 
-@jax.partial(jax.profiler.trace_function, name="sample_action")
+def predict_action_values(q_state, state, actions):
+    """Predict the value of each of the given `actions` in `state`.
+    Returns:
+    - an ndarray of dimension `actions.shape[0]` containing the value of each
+        action
+    """
+    n_candidates = actions.shape[0]
+    values = predict_value(q_state,
+                           jnp.repeat(jnp.expand_dims(state, 0),
+                                      n_candidates, axis=0),
+                           actions.reshape(n_candidates, -1))
+    return values.reshape(-1)
+# takes a batch of states and a (batch x n_candidates) of actions
+# -> a (batch x n_candidates) of values
+predict_action_values_batch = jax.vmap(predict_action_values,  # noqa: E305
+                                       in_axes=(None, 0, 0))
+
+
 @jax.jit
-def sample_action(q_state: QLearnerState, rng, state, actions):
-    values = predict(q_state,
-                     jnp.repeat(state.reshape(1, *state.shape),
-                                actions.shape[0], axis=0),
-                     actions.reshape(4, 1))
-    values = values.reshape(-1)
-    boltzmann = True
-    if boltzmann:
-        action = sample_boltzmann(rng, values, actions)
-    else:
-        action = sample_egreedy(rng, values, actions)
-    return action
-sample_action_n = jax.vmap(sample_action,  # noqa: E305
-                           in_axes=(0, None, None, None))
+def sample_action_egreedy(q_state: QLearnerState, rng, state, actions, epsilon):
+    values = predict_action_values(q_state, state, actions)
+    action = sample_egreedy(rng, values, actions, epsilon=epsilon)
+    return action, values
+sample_action_egreedy_n = jax.vmap(sample_action_egreedy,  # noqa: E305
+                                   in_axes=(None, 0, None, None, None))
 
 
-def sample_boltzmann(rng, values, actions, temperature=1):
-    boltzmann_probs = nn.softmax(values / temperature)
+@jax.jit
+def sample_action_boltzmann(q_state: QLearnerState, rng, state, actions, temp):
+    values = predict_action_values(q_state, state, actions)
+    action = sample_boltzmann(rng, values, actions)
+    return action, values
+sample_action_boltzmann_n = jax.vmap(sample_action_boltzmann,  # noqa: E305
+                                     in_axes=(None, 0, None, None, None))
+
+
+@jax.jit
+def sample_boltzmann(rng, values, actions, temp=1):
+    boltzmann_probs = nn.softmax(values / temp)
     sampled_index = random.categorical(rng, boltzmann_probs)
     action = actions[sampled_index]
     return action
 sample_boltzmann_n = jax.vmap(sample_boltzmann,  # noqa: E305
-                              in_axes=(0, None, None))
+                              in_axes=(0, None, None, None))
 
 
-def sample_egreedy(rng, values, actions, epsilon=0.05):
+@jax.jit
+def sample_egreedy(rng, values, actions, epsilon=0.5):
     explore = random.bernoulli(rng, p=epsilon)
     rng = random.split(rng, 1)[0]
     random_index = random.randint(rng, (1,), 0, actions.shape[0])[0]
@@ -120,53 +207,126 @@ def sample_egreedy(rng, values, actions, epsilon=0.05):
                       None)
     return action
 sample_egreedy_n = jax.vmap(sample_egreedy,  # noqa: E305
-                            in_axes=(0, None, None))
+                            in_axes=(0, None, None, None))
 
 
-if __name__ == '__main__':
+def location_value(q_state: QLearnerState, env: gridworld.GridWorld, location):
+    env = env.replace(agent=jnp.array(location))
+    s = env.render(env.agent)
+    values = predict_action_values(q_state, s, env.actions)
+    return jnp.max(values)
+location_value_batch = jax.vmap(location_value,  # noqa: E305
+                                in_axes=(None, None, 0))
+
+
+def render_value_map(q_state: QLearnerState, env: gridworld.GridWorld):
+    locations = gridworld.all_coords(env.size)
+    location_values = location_value_batch(q_state, env, locations)
+    value_map = np.zeros((env.size, env.size))
+    for location, value in zip(locations, location_values):
+        # import ipdb; ipdb.set_trace()
+        value_map[location[0], location[1]] = value
+    return value_map
+
+
+def display_value_map(q_state, env):
+    value_map = render_value_map(q_state, env)
+    fig, ax = plt.subplots()
+    img = ax.imshow(value_map)
+    fig.colorbar(img, ax=ax)
+    fig.show()
+    plt.close(fig)
+
+
+def main(args):
     rng = random.PRNGKey(0)
-    env = gridworld.new(5)
+    env = gridworld.new(args.env_size)
     state_shape = (2, env.size)
     action_shape = (1,)
     batch_size = 128
     max_steps = 100
 
-    q_state = init_fn(0, (128, *state_shape), (128, *action_shape))
+    if args.boltzmann:
+        sample_action = jax.partial(sample_action_boltzmann, temp=1)
+    else:
+        sample_action = jax.partial(sample_action_egreedy, epsilon=0.5)
+
+    q_state = init_fn(0, (128, *state_shape), (128, *action_shape),
+                      tabular=args.tabular, env_size=args.env_size)
     targetq_state = q_state
     replay = replay_buffer.Replay(state_shape, action_shape)
+    candidate_actions = jnp.tile(jnp.expand_dims(env.actions, 0),
+                                 (batch_size, 1))
 
-    # @jax.jit
-    # @jax.profiler.trace_function
-    def full_step(q_state, rng, env):
+    def full_step(q_state, targetq_state, rng, env, train=True):
         s = gridworld.render(env)
-        a = sample_action(q_state, rng, s, env.actions)
-        # with profiler.TraceContext("env step"):
-        env, sp, r = gridworld.step(env, int(a))
-        # with profiler.TraceContext("replay append"):
-        replay.append(s, a, sp, r)
+        if train:
+            a, v = sample_action(q_state, rng, s, env.actions)
+        else:
+            a, v = sample_action_egreedy(q_state, rng, s, env.actions, 0.01)
 
-        if len(replay) > batch_size:
-            # with profiler.TraceContext("replay sample"):
-            transitions = replay.sample(batch_size)
-            # with profiler.TraceContext("bellman step"):
-            q_state = bellman_train_step(q_state, targetq_state, transitions)
+        env, sp, r = gridworld.step(env, int(a))
+
+        if train:
+            replay.append(s, a, sp, r)
+            if len(replay) > batch_size:
+                transitions = replay.sample(batch_size)
+                if args.tabular:
+                    q_state = tabular_train_step(q_state, targetq_state,
+                                                 transitions, candidate_actions)
+                else:
+                    q_state = bellman_train_step(q_state, targetq_state,
+                                                 transitions, candidate_actions)
+
         return q_state, env, r
 
     # @jax.profiler.trace_function
-    def run_episode(rngs, q_state, env):
+    def run_episode(rngs, q_state, targetq_state, env, train=True):
         env = gridworld.reset(env)
         score = 0
         for i in range(max_steps):
-            q_state, env, r = full_step(q_state, rngs[i], env)
+            q_state, env, r = full_step(
+                q_state, targetq_state, rngs[i], env, train)
             score += r
         return q_state, env, score
 
-    for episode in range(200):
+    for episode in range(1000):
         rngs = random.split(rng, max_steps + 1)
         rng = rngs[0]
-        q_state, env, score = run_episode(rngs[1:], q_state, env)
-        print(f"Episode {episode}, Score {score}")
+        q_state, env, score = run_episode(rngs[1:], targetq_state, q_state, env)
+
+        rngs = random.split(rng, max_steps + 1)
+        rng = rngs[0]
+        _, _, test_score = run_episode(rngs[1:], targetq_state, q_state, env,
+                                       train=False)
+        print((f"Episode {episode:4d}"
+               f", Train score {score:3d}"
+               f", Test score {test_score:3d}"))
+        if episode % 50 == 0:
+            print("\nQ network values")
+            display_value_map(q_state, env)
+            # import ipdb; ipdb.set_trace()
+            # print("\nTarget Q network values")
+            # display_value_map(targetq_state, env)
         if episode % 1 == 0:
             targetq_state = q_state
 
-    profiler.save_device_memory_profile("memory.prof")
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tabular', action='store_true', default=False)
+    parser.add_argument('--env_size', type=int, default=5)
+    parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--boltzmann', action='store_true', default=False)
+    args = parser.parse_args()
+
+    jit = not args.debug
+    if jit:
+        main(args)
+    else:
+        with jax.disable_jit():
+            main(args)
+
+    # profiler.save_device_memory_profile("memory.prof")

utils.py
@@ -7,20 +7,6 @@ from jax.lib import pytree
 def one_hot(x, k, dtype=jnp.int16):
     """Create a one-hot encoding of x of size k."""
     return jnp.array(x[:, None] == jnp.arange(k), dtype)
-# # one_hot = jax.jit(one_hot, static_argnums=(1, 2))  # noqa: E305
-# # one_hot_10 = jax.partial(one_hot, k=10)  # noqa: E305
-
-# one_hot_10 = lambda x: one_hot(x, 10)
-
-# if __name__ == "__main__":
-#     print(one_hot(jnp.array((2,)), 10))
-#     # one_hot_batch = jax.vmap(one_hot)
-#     # print(one_hot_batch(jnp.array([[2, 3, 4]]), jnp.array([[10, 10, 10]])))
-
-#     # one_hot_batch = jax.vmap(one_hot_10)
-#     # print(one_hot_batch(jnp.array([[2, 3, 4]])))
-#     one_hot_batch = jax.vmap(one_hot_10)
-#     print(one_hot_batch(jnp.array([[2, 3, 4]])))
 
 
 def tree_stack(trees):
@@ -59,6 +45,11 @@ def tree_unstack(tree):
             new_leaves[i].append(leaf[i])
     new_trees = [treedef.unflatten(l) for l in new_leaves]
     return new_trees
+
+
+# def all_pairs(a, b):
+#     grid = jnp.stack(jnp.meshgrid(a, b))
+#     return grid.transpose().reshape(-1, 2).astype(DTYPE)
 
 
 if __name__ == "__main__":